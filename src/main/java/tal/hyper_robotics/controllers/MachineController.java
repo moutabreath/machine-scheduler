b021e317fe1431198a7568617b343286538772f3
moutabreath <tdruck@gmail.com>
2025-02-21 15:20:28 +0200

    remove dummy test update gitignore use SSEemitter

--- src/main/java/tal/hyper_robotics/controllers/MachineController.java
+++ src/main/java/tal/hyper_robotics/controllers/MachineController.java
@@ -1,9 +1,11 @@
 package tal.hyper_robotics.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import tal.hyper_robotics.scheduler.JobScheduler;
 
@@ -13,9 +15,17 @@ public class MachineController {
 
     @Autowired
     private JobScheduler jobScheduler;
-    
+
     @PostMapping("/startJobs")
-    public void startJobs(){
+    public void startJobs() {
         jobScheduler.scheduleJobs();
     }
+
+    @GetMapping("/jobUpdated/register")
+    public SseEmitter streamJobsAdded() {
+        return jobScheduler.streamJobsAdded();
+
+    }
+
+
 }
--- src/main/java/tal/hyper_robotics/scheduler/JobScheduler.java
+++ src/main/java/tal/hyper_robotics/scheduler/JobScheduler.java
@@ -1,7 +1,9 @@
 package tal.hyper_robotics.scheduler;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -9,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import tal.hyper_robotics.entities.Job;
 
 @Service
@@ -21,6 +24,7 @@ public class JobScheduler {
     private final ExecutorService executorService;
     private static final Logger logger = LoggerFactory.getLogger(JobScheduler.class);
     private final int NUM_OF_CONCURRENT_JOBS_PROCESSES = 2;
+    private final CopyOnWriteArrayList<SseEmitter> emitters = new CopyOnWriteArrayList<>();
 
     public JobScheduler(){
         machineA = new MachineProcessor("A", 1, 10, logger);
@@ -31,27 +35,51 @@ public class JobScheduler {
     }
 
     public void scheduleJobs() {
-        executorService.submit(() -> {
-            scheduleJobsInternal();
-        });
+        executorService.submit(this::scheduleJobsInternal);
+    }
+
+    private void handleUserCreatedEvent(Job job) {
+        for (SseEmitter emitter : emitters) {
+            try {
+                emitter.send(SseEmitter.event().name("jobUpdated").data(job));
+                logger.info("emitted job change for {}", job.getId());
+            } catch (IOException e) {
+                emitters.remove(emitter);
+            }
+        }
     }
 
     private void scheduleJobsInternal() {
         Job job = new Job();
+
         machineA.addJob(job);
+        handleUserCreatedEvent(job);
         logger.info("JobScheduler Machine A started processing job {}", job.getId());
         Job machineADoneJob = machineA.getFinishedJob();
+        handleUserCreatedEvent(machineADoneJob);
         logger.info("JobScheduler Machine A finished processing job {}", machineADoneJob.getId());
 
         machineB.addJob(machineADoneJob);
+        handleUserCreatedEvent(machineADoneJob);
         Job machineBDoneJob = machineB.getFinishedJob();
+        handleUserCreatedEvent(machineBDoneJob);
         logger.info("JobScheduler Machine B finished processing job {}", machineADoneJob.getId());
 
         machineC.addJob(machineBDoneJob);
+        handleUserCreatedEvent(machineBDoneJob);
         Job machineCDoneJob = machineC.getFinishedJob();
         logger.info("JobScheduler Machine C finished processing job {}", machineCDoneJob.getId());
         doneJobs.add(machineCDoneJob);
+        handleUserCreatedEvent(machineCDoneJob);
     }
 
 
+    public SseEmitter streamJobsAdded() {
+        SseEmitter emitter = new SseEmitter();
+        emitters.add(emitter);
+        emitter.onCompletion(() -> emitters.remove(emitter));
+        emitter.onTimeout(() -> emitters.remove(emitter));
+        emitter.onError((e) -> emitters.remove(emitter));
+        return emitter;
+    }
 }
